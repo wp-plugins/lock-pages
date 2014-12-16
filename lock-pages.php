8fad28c774b7eb1d01ec1ae6871c4f2a00277902
gyrus <gyrus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-16 22:04:26 +0000

    Version 0.2.3
    git-svn-id: https://plugins.svn.wordpress.org/lock-pages/trunk@1046211 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lock-pages.php
+++ lock-pages.php
@@ -4,7 +4,7 @@ Plugin Name: Lock Pages
 Plugin URI: http://wordpress.org/extend/plugins/lock-pages/
 Description: Allows admins to lock pages in order to prevent breakage of important URLs.
 Author: Steve Taylor
-Version: 0.2.2
+Version: 0.2.3
 Author URI: http://sltaylor.co.uk
 Based on: http://pressography.com/plugins/wordpress-plugin-template/
 */
@@ -47,11 +47,6 @@ if ( ! class_exists('SLT_LockPages') ) {
 		* @var	array		$options	Stores the options for this plugin
 		*/
 		var $options = array();
-
-		/**
-		* PHP 4 Compatible Constructor
-		*/
-		function SLT_LockPages() { $this->__construct(); }
 
 		/**
 		* PHP 5 Constructor
